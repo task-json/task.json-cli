4cd7bda9aaac3380db8dac46c9b8070fa75296c0
DCsunset <dcsunset@protonmail.com>
2020-12-23 17:41:21 +0800

    feat: add rm command

---
+++ src/commands/rm.ts
@@ -0,0 +1,61 @@
+import {Command, flags} from '@oclif/command'
+import * as fs from "fs";
+import { format } from "date-fns";
+import { readTasks, writeTasks } from "../utils/task";
+import { readConfig } from "../utils/config";
+import * as _ from "lodash";
+
+export default class Remove extends Command {
+  static description = 'Delete tasks';
+
+  static examples = [
+    `$ todo rm 1`,
+  ];
+
+  static flags = {
+    help: flags.help({ char: 'h' }),
+    done: flags.boolean({
+      char: "d",
+      description: "delete done tasks"
+    })
+  };
+
+  // Allow multiple arguments
+  static strict = false;
+
+  static args = [{
+    name: "ID...",
+    description: "delete specific tasks"
+  }];
+
+  async run() {
+    const { argv, flags } = this.parse(Remove);
+    const { todoPath, donePath } = readConfig();
+
+    const taskPath = flags.done ? donePath : todoPath;
+
+    if (!fs.existsSync(taskPath)) {
+      this.error(`${flags.done ? "done" : "todo"}.json does not exist.`);
+    }
+
+    // Read Todo
+    const tasks = readTasks(taskPath);
+
+    const ids = argv.map(a => {
+      const id = parseInt(a);
+      if (isNaN(id) || id <= 0)
+        this.error("Invalid IDs");
+      if (id > tasks.length)
+        this.error(`Task ${id} does not exist.`);
+      return id - 1;
+    });
+
+    const removedTasks = _.remove(tasks, (_, index) => ids.includes(index)).map(task => {
+      task.end = format(new Date(), "yyyy-MM-dd")
+      return task;
+    });
+    writeTasks(taskPath, tasks);
+
+    this.log(`Delete ${removedTasks.length} task(s) from ${flags.done ? "done" : "todo"}.json.`);
+  }
+}
