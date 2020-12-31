1f075aaaffe1cf3addc06f1d63659206458ace9e
DCsunset <dcsunset@protonmail.com>
2020-12-31 17:30:47 +0800

    feat: add lsid command

---
+++ src/commands/lsid.ts
@@ -0,0 +1,49 @@
+import {Command, flags} from '@oclif/command'
+import * as fs from "fs";
+import { readTasks } from "../utils/task";
+import { readConfig } from "../utils/config";
+import { Task } from "../types";
+import * as _ from "lodash";
+
+export default class ListProj extends Command {
+  static description = 'List IDs'
+
+  static examples = [
+    `$ td lsid`,
+    `$ td lsid -D`
+  ]
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+    done: flags.boolean({
+      char: "D",
+      description: "list projects of only done tasks"
+    })
+  }
+
+  async run() {
+    const { flags } = this.parse(ListProj);
+    const { todoPath, donePath } = readConfig();
+
+    let size = 0;
+
+    if (!flags.done) {
+      let todoTasks = [] as Task[];
+      if (fs.existsSync(todoPath)) {
+        todoTasks = readTasks(todoPath);
+      }
+      size = todoTasks.length;
+    }
+
+    if (flags.done) {
+      let doneTasks = [] as Task[];
+      if (fs.existsSync(donePath)) {
+        doneTasks = readTasks(donePath);
+      }
+
+      size = doneTasks.length;
+    }
+
+    this.log(_.range(1, size + 1).join("\n"));
+  }
+}
