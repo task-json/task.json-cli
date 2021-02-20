e6f4b0390b461c0af5a92313a88c2542db737223
DCsunset <dcsunset@protonmail.com>
2021-02-20 15:18:27 +0800

    feat: allow undo removed tasks

--- src/commands/undo.ts
+++ src/commands/undo.ts
@@ -2,16 +2,22 @@ import {Command, flags} from '@oclif/command'
 import { parseIds, readTaskJson, writeTaskJson } from "../utils/task";
 import { checkTaskExistence } from "../utils/config";
 import * as _ from "lodash";
+import { TaskType } from "task.json";
 
 export default class Undo extends Command {
   static description = 'Undo tasks';
 
   static examples = [
     `$ tj undo 1 2`,
+    `$ tj undo --removed 1 2`,
   ];
 
   static flags = {
-    help: flags.help({ char: 'h' })
+    help: flags.help({ char: 'h' }),
+		removed: flags.boolean({
+			char: "r",
+			description: "restore removed tasks"
+		})
   };
 
   // Allow multiple arguments
@@ -19,18 +25,19 @@ export default class Undo extends Command {
 
   static args = [{
     name: "ID...",
-    description: "undo specific done tasks"
+    description: "undo specific done or removed tasks"
   }];
 
   async run() {
-    const { argv } = this.parse(Undo);
+    const { argv, flags } = this.parse(Undo);
 
     checkTaskExistence(this.error);
 
     const taskJson = readTaskJson();
-    const ids = parseIds(argv, taskJson.done.length, this.error);
+		const type: TaskType = flags.removed ? "removed" : "done";
+    const ids = parseIds(argv, taskJson[type].length, this.error);
 
-    const undoneTasks = _.remove(taskJson.done, (_, index) => ids.includes(index)).map(task => {
+    const undoneTasks = _.remove(taskJson[type], (_, index) => ids.includes(index)).map(task => {
       delete task.end;
       task.modified = new Date().toISOString();
       return task;
