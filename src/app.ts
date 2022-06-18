be5fe219321ffe8437abed7fcc1677139695d7ae
DCsunset <DCsunset@users.noreply.github.com>
2022-06-18 10:28:29 -0400

    feat!: add basic framework using commander

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+/**
+ * Copyright (C) 2020-2022 DCsunset
+ * See full notice in README.md in this project
+ */
+
+import { Command } from "commander";
+import { version } from "../package.json";
+
+const program = new Command();
+
+program
+	.name("tj")
+	.description("Command line todo management app based on task.json format")
+	.version(version);
+
+program.parse();
